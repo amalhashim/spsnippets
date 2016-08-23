b21583810f3f1c34d2798eba4f8e981bbd7874d0
Amal Hashim <amalhashim@gmail.com>
2016-08-23 20:07:35 -0300

    Update psCommands.ps1

--- psCommands.ps1
+++ psCommands.ps1
@@ -1,3 +1,3 @@
-Powershell Script - Find log information for specific co-relation ID
+#Powershell Script - Find log information for specific co-relation ID
 
 Merge-SPLogFile -Path "C:\Users\amalhashim\Desktop\MergedLogs.log" -Correlation 28b89c9d-ad4e-300f-32e-60ea97196122 -Overwrite
