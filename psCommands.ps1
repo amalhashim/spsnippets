9e50751644a521866215cd57e4dde563012177fb
Amal Hashim <amalhashim@gmail.com>
2016-10-28 10:20:53 -0300

    Update psCommands.ps1

--- psCommands.ps1
+++ psCommands.ps1
@@ -1,3 +1,8 @@
 #Powershell Script - Find log information for specific co-relation ID
 
 Merge-SPLogFile -Path "C:\Users\amalhashim\Desktop\MergedLogs.log" -Correlation 28b89c9d-ad4e-300f-32e-60ea97196122 -Overwrite
+
+#Powershell Script - Create host header sitecollection
+
+New-SPSite http://mysite.com -HostHeaderWebApplication http://mywebapp.com/ -Name 'Site Title' 
+-ContentDatabase WSS_Content_MySite -OwnerAlias 'domain\amalhashim' -language 1033 -Template 'STS#0'
